0771092d5e023b9df418ca51076ac59d87c5095d
oladimeji <tommyslipz@gmail.com>
2023-06-05 21:23:03 +0100

    completed-header page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 import generateJoke from './generateJoke'
 import './styles/main.scss'
 import laughing from './assets/laughing.svg'
+import f1 from './assets/f1.jpeg'
 
+console.log(f1, "image")
+const im = document.getElementById('f1')
+im.src = f1
 const laughImg = document.getElementById('laughImg')
 laughImg.src = laughing
-
-const jokeBtn = document.getElementById('jokeBtn')
-jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
